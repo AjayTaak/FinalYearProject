1f8edbbc2cda47f8baa6c64d1620a6203e2be1ed
AjayTaak <ajmehr.taak@gmail.com>
2021-01-23 16:45:32 +0000

    Test Commit

--- app/src/main/java/com/example/finalyearproject/MainActivity.java
+++ app/src/main/java/com/example/finalyearproject/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.finalyearproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "Test Run", Toast.LENGTH_SHORT).show();
     }
 }
